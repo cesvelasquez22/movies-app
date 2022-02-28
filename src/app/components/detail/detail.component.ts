3f525c95964886a392700db4b7bc82a5a43b522d
Cesar Velásquez Córdova <ces.velasquez@hotmail.com>
2022-02-27 21:12:30 -0600

    + Mostrar detalles de la pelicula - Parte 1

--- src/app/components/detail/detail.component.ts
+++ src/app/components/detail/detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { MovieDetail } from '@interfaces';
 import { MoviesService } from '@services';
 
 @Component({
@@ -8,11 +9,14 @@ import { MoviesService } from '@services';
 })
 export class DetailComponent implements OnInit {
   @Input() id: number;
+  movieDetail: MovieDetail;
 
   constructor(private _moviesService: MoviesService) {}
 
   ngOnInit() {
-    this._moviesService.getMovieDetail(this.id).subscribe(console.log);
+    this._moviesService
+      .getMovieDetail(this.id)
+      .subscribe((detail) => (this.movieDetail = detail));
     this._moviesService.getMovieCredits(this.id).subscribe(console.log);
   }
 }
